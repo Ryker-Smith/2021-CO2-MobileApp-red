d60489ea3746016b59a2f4664a2723f82c3c588c
Ryker-Smith <fachtnaroe@gmail.com>
2021-12-20 12:53:25 +0000

    Initial commit, testing repository connection.

---
+++ app/src/main/java/net/fachtnaroe/co2_mobileapp_green/arduino_eeprom_data.java
@@ -0,0 +1,13 @@
+package net.fachtnaroe.co2_mobileapp_green;
+class arduino_eeprom_data {
+        private static final int max_SSID = 32;
+        private static final int max_PSK = 64;
+        private static final int max_DeviceName = 32;
+
+        public char active;
+        public byte config_Status;
+        public byte config_Attempts;
+        public char[] config_SSID = new char[max_SSID];
+        public char[] config_PSK = new char[max_PSK];
+        public char[] config_DeviceName = new char[max_DeviceName];
+}
